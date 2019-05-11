d95921d6b0984abb5b35fd6dd578eccf2814c24f
Alvin <ling94alvin@gmail.com>
2019-05-11 12:26:58 -0500

    add custom cell classes

---
+++ iOS-Chatty/ChatCell.swift
@@ -0,0 +1,26 @@
+//
+//  ChatCell.swift
+//  iOS-Chatty
+//
+//  Created by Alvin Ling on 5/11/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+    
+    static let reuseID = "ChatCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS-Chatty/UserCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserCell.swift
+//  iOS-Chatty
+//
+//  Created by Alvin Ling on 5/11/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UICollectionViewCell {
+    static let reuseID = "UserCell"
+}
